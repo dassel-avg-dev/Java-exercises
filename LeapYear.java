ef4fe425c379fdafae84abd48e0596f16923c1aa
Dassel Bacaltos <dasselescalante013@gmail.com>
2025-09-09 13:22:43 +0000

    initial commit

---
+++ LeapYear.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter starting year: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter end year: ");
+        int end = scanner.nextInt();
+
+        if(start > end) {
+            System.out.println("Invalid starting year.");
+            System.exit(1);
+        }
+
+        int leapCount=0;
+        int notLeapCount=0;
+
+        System.out.println("Leap years from " + start + " to " + end);
+        while(start <= end) {
+            if(start % 4 == 0 && start % 100 != 0) {
+                leapCount++;
+                System.out.println(start);
+            } 
+            else if(start % 400 == 0 && start % 100 != 0) {
+                leapCount++;
+                System.out.println(start);
+            }
+            else {
+                notLeapCount++;
+            }
+
+            start++;
+        }
+
+        System.out.println("Number of leap years: " + leapCount);
+        System.out.println("Number of not leap years: " + notLeapCount);
+
+
+        scanner.close();
+    }
+}
